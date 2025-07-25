bfbdb6fd39c5075828f9aee8186ee3360f45910f
restarea92 <restarea@me.com>
2025-07-25 17:53:31 +0900

    force rebuild

--- visual-scroll.js
+++ visual-scroll.js
@@ -97,7 +97,7 @@ const app = {
 
         const resizeObserver = new ResizeObserver(() => {
             const height = stickyElement.getBoundingClientRect().height;
-            stickyWrapper.style.height = `${height * 2}px`;
+            stickyWrapper.style.height = `${height * 1.5}px`;
         });
 
         resizeObserver.observe(stickyElement);
