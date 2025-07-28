eb97afbe6061b60e1e4bf0d03d542c02d2ade5ca
restarea92 <restarea@me.com>
2025-07-28 19:13:15 +0900

    force rebuild

--- visual-scroll.js
+++ visual-scroll.js
@@ -49,7 +49,7 @@ const app = {
         scrollTimer: null,
         progress: 0,
         isActive: null,
-        version: '1.0.22',
+        version: '1.0.23',
         updateProgressCallCount: 0  // 호출 횟수 카운터
     },
 
