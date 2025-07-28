204e4d71f5a8d19265a74ef5c7fb22ac5ff74bb2
restarea92 <restarea@me.com>
2025-07-28 17:45:12 +0900

    force rebuild

--- visual-scroll.js
+++ visual-scroll.js
@@ -49,7 +49,7 @@ const app = {
         scrollTimer: null,
         progress: 0,
         isActive: null,
-        version: '1.0.12'
+        version: '1.0.13'
     },
 
     // Configuration constants
