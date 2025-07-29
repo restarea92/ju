f4c2b7399eba4e187b8883d4995254fbfa651792
restarea92 <restarea@me.com>
2025-07-29 15:26:14 +0900

    force rebuild

--- visual-scroll.js
+++ visual-scroll.js
@@ -46,7 +46,7 @@ if (typeof gsap !== 'undefined') {
 const app = {
     // ========== 상수 (CONFIG) ==========
     CONFIG: {
-        VERSION: '1.0.61',
+        VERSION: '1.0.62',
         ACTIVATION_THRESHOLD: 15,
         SCROLL_DEBOUNCE_DELAY: 150,
         STICKY_HEIGHT_MULTIPLIER: 1.75,
