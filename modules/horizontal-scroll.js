35bac51f17f6fb7bf68d4f9bc79bf4cac47dbd18
restarea92 <restarea@me.com>
2025-08-04 09:31:05 +0900

    refeash cache

--- modules/horizontal-scroll.js
+++ modules/horizontal-scroll.js
@@ -10,7 +10,7 @@ const app = {
 
     // ========== Configuration ==========
     CONFIG: {
-        VERSION: '1.0.1',
+        VERSION: '1.0.2',
     },
 
     // ========== Initialization ==========
