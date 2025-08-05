245188ccd07c0d8dc5325826b6fca27f961ac339
restarea92 <restarea@me.com>
2025-08-05 16:06:57 +0900

    refeash cache

--- modules/rect-scroll.js
+++ modules/rect-scroll.js
@@ -76,21 +76,21 @@ const app = {
                 "--rect-scroll-progress": 1,
             }, 0);
 
-            this.createTimeline({
-                start:"top bottom",
-                end:"top top",
-                scrub:true,
-            }).to(background, {
-                "--rect-scroll-brightness": 0.7,
-            }, 0);
+            // this.createTimeline({
+            //     start:"top bottom",
+            //     end:"top top",
+            //     scrub:true,
+            // }).to(background, {
+            //     "--rect-scroll-brightness": 0.7,
+            // }, 0);
 
-            this.createTimeline({
-                start:"top top",
-                end:"bottom top",
-                scrub:1,
-            }).to(background, {
-                "--rect-scroll-brightness": 0.25,
-            }, 0);
+            // this.createTimeline({
+            //     start:"top top",
+            //     end:"bottom top",
+            //     scrub:1,
+            // }).to(background, {
+            //     "--rect-scroll-brightness": 0.25,
+            // }, 0);
 
         }
 
