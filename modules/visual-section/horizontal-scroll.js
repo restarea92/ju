b0c1f70b1b44c6dd859a02f33968e3777179053e
restarea92 <restarea@me.com>
2025-08-12 16:22:46 +0900

    refeash cache

--- modules/visual-section/horizontal-scroll.js
+++ modules/visual-section/horizontal-scroll.js
@@ -4,7 +4,7 @@
  * @version 1.0.9
  */
 
-import { initGSAP } from './gsapUtils.js';
+import { initGSAP } from '../gsapUtils.js';
 
 const app = {
 
--- modules/visual-section/rect-scroll.js
+++ modules/visual-section/rect-scroll.js
@@ -4,7 +4,7 @@
  * @version 1.0.7
  */
 
-import { initGSAP } from './gsapUtils.js';
+import { initGSAP } from '../gsapUtils.js';
 
 const app = {
     // ========== Configuration ==========
